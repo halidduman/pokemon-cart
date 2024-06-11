dcb3ef60fc54b95a2bdcdbca7a268bb5cfa59e85
halid <halidduman60@gmail.com>
2024-06-11 17:02:42 +0300

    js-start

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ const poke_container= document.querySelector(".poke-container")
 const search = document.querySelector(".search")
 const searchBtn = document.querySelector(".searchBtn")
 const searchInput = document.querySelector(".search-Input")
+const pokeLogo = document.getElementById('poke-logo');
+const red =document.querySelector(".redShadow")
+
 
 searchBtn.addEventListener("click", ()=>{
     search.classList.toggle("active")
 })
+
+
