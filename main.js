3e57cd303f5ba8c1950ac9bbc4ce1be39b99c0b1
halid <halidduman60@gmail.com>
2024-06-11 19:33:38 +0300

    update-3

--- main.js
+++ main.js
@@ -2,12 +2,27 @@ const poke_container= document.querySelector(".poke-container")
 const search = document.querySelector(".search")
 const searchBtn = document.querySelector(".searchBtn")
 const searchInput = document.querySelector(".search-Input")
-const pokeLogo = document.getElementById('poke-logo');
-const red =document.querySelector(".redShadow")
+const pokemon_count  = 151
 
-
+// -----------------------------------------------
 searchBtn.addEventListener("click", ()=>{
     search.classList.toggle("active")
 })
 
+const fetchPokemons = async()=> {
+    for( let i = 1; i <pokemon_count; i++){
+        await getPokemon(i)
+    }
+}
 
+const getPokemon = async (id) =>{
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    // console.log(data)
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) =>  {}
+
+fetchPokemons()
