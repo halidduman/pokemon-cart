6fb886213f9e21d76cd41191ca42ccdf77ffd51e
halid <halidduman60@gmail.com>
2024-06-11 15:50:54 +0300

    css-update

---
+++ main.js
@@ -0,0 +1,8 @@
+const poke_container= document.querySelector(".poke-container")
+const search = document.querySelector(".search")
+const searchBtn = document.querySelector(".searchBtn")
+const searchInput = document.querySelector(".search-Input")
+
+searchBtn.addEventListener("click", ()=>{
+    search.classList.toggle("active")
+})
